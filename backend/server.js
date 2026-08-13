c46e17ec7c43b4d95d7e66f0af4e37e7b3a7c732
SONI718605 <maharshsoni5@gmail.com>
2026-08-13 23:30:39 +0530

    Allow MONGODB_URI database variable fallback

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || process.env.MONGODB_URI;
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
